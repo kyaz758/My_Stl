1773659bc92dcb2368a22ab4662be0c18ad19226
Clang <kyaz758@gmail.com>
2017-01-16 16:40:32 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,14 @@
+/******************************************************************************
+ ***** File Name : test.cpp
+ **** Created By : kyaz758
+ *** Description :
+ * Creation Date : 2017-01-16 16:39
+ * Last Modified : 2017-01-16 16:39
+ ******************************************************************************/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+//Test
